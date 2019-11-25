93c4f4cb0c10f80299a4c17554286847f2d5cae6
Tyisha <tlwilliams895@gmail.com>
2019-11-25 14:14:08 -0500

    Initial commit - 11/25/2019 - TLW

---
+++ src/main/java/org/launchcode/Spark_Effortless_Banking/SparkEffortlessBankingApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Spark_Effortless_Banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkEffortlessBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkEffortlessBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Spark_Effortless_Banking/SparkEffortlessBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Spark_Effortless_Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkEffortlessBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
