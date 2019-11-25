e7fb7f2bc86988b18187fcc07708553f30f473bb
Tyisha <tlwilliams895@gmail.com>
2019-11-25 15:51:45 -0500

    Project Setup Updates - 11/25/2019 - TLW

---
+++ src/main/java/org/launchcode/Spark_Effortless_Banking/Controllers/spark_effortless_banking.java
@@ -0,0 +1,10 @@
+package org.launchcode.Spark_Effortless_Banking.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class spark_effortless_banking {
+
+}
